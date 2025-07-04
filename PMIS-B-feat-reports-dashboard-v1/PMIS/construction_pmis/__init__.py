35d52296f3b233508b795f01b64e91547694799b
Bereketeab-dev <142420230+Bereketeab-dev@users.noreply.github.com>
2025-07-04 20:56:01 +0300

    Delete PMIS-B-feat-reports-dashboard-v1/PMIS directory

--- PMIS-B-feat-reports-dashboard-v1/PMIS/construction_pmis/__init__.py
+++
@@ -1,2 +0,0 @@
-# __init__.py for construction_pmis app
-__version__ = "0.0.1"
--- PMIS-B-feat-reports-dashboard-v1/PMIS/construction_pmis/doctype/project_resource_plan_item/project_resource_plan_item.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) [Your Name/Organization] and contributors. For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class ProjectResourcePlanItem(Document):
-    pass
